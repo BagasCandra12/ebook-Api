45c65e50fd970e36dcab3630730149f2f1125cf9
bagas candra <Bagascandra577@gmail.com>
2020-10-10 00:16:53 +0700

    Uploud Endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function me(){
+        return "NIS : 3103118032
+                NAMA : Bagas Candra Pratama 
+                GENDER : LAKI - LAKI
+                PHONE : 0895368475809
+                KELAS : XI RPL 3";
+    }
+}
